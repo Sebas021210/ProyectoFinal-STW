484c3f48250650566c55408868661b9ea368593d
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-06-01 03:19:36 -0600

    Webpack

---
+++ portafolio/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            chunks: ['index']
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new CssMinimizerPlugin()
+    ],
+    optimization: {
+        minimizer: [new CssMinimizerPlugin(),],
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json']
+    },
+};
