463c249d9b4716d90f409ad9d2b2790006b994ae
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-05-31 22:26:42 -0600

    Arreglos

--- portafolio/src/components/cards.js
+++ portafolio/src/components/cards.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Icon from '@mdi/react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -14,6 +15,16 @@ import Webpack from '../images/Webpack.png';
 import Babel from '../images/Babel.png';
 import github from '../images/github.png';
 import link from '../images/link.png';
+import { mdiLanguageHtml5 } from '@mdi/js';
+import { mdiLanguageJavascript } from '@mdi/js';
+import { mdiLanguageCss3 } from '@mdi/js';
+import { mdiVuejs } from '@mdi/js';
+import { mdiReact } from '@mdi/js';
+import { mdiWebpack } from '@mdi/js';
+import { mdiBabel } from '@mdi/js';
+import { mdiVuetify } from '@mdi/js';
+import { mdiSass } from '@mdi/js';
+import "./styles/cards.css"
 
 function MyVerticallyCenteredModal({ card, ...props }) {
     return (
@@ -52,56 +63,64 @@ function Cards() {
             text: "El laboratorio consiste en eligir una imagen y reproducirla utilizando únicamente html y css.",
             image: labCss,
             githubLink: "https://github.com/Sebas021210/LabCSS",
-            link: "http://3.15.17.61:3162/"
+            link: "http://3.15.17.61:3162/",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3]
         },
         {
             title: "Laboratorio Chat",
             text: "El laboratorio consiste en realizar el diseño de un chat que servira para conectarse a un servidor en el que se podrá hacer uso de un chat en tiempo real.",
             image: labChat,
             githubLink: "https://github.com/Sebas021210/LabChat",
-            link: "http://3.15.17.61:3160/"
+            link: "http://3.15.17.61:3160/",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3, mdiLanguageJavascript]
         },
         {
             title: "Laboratorio Webpack and SASS/SCSS",
             text: "El laboratorio consiste en transformar una historia en la que se utilice JS, CSS, HTML, con el fin de crear un proyecto modular para poder aplicar Webpack.",
             image: Webpack,
             githubLink: "https://github.com/Sebas021210/Lab7SW/tree/main/Lab7STW",
-            link: ""
+            link: "",
+            icons: [mdiLanguageHtml5, mdiSass, mdiLanguageJavascript, mdiWebpack]
         },
         {
             title: "Laboratorio Webpack with Babel",
             text: "El laboratorio consiste en tener una fase final donde se muestre la configuración de los JS generados por medio de babel.",
             image: Babel,
             githubLink: "https://github.com/Sebas021210/Lab7SW/tree/main/Lab8Babel/Lab7STW",
-            link: ""
+            link: "",
+            icons: [mdiLanguageHtml5, mdiSass, mdiLanguageJavascript, mdiWebpack, mdiBabel]
         },
         {
             title: "Laboratorio React",
             text: "El laboratorio consiste en realizar un juego de memoria en una grid de al menos 4x4 tarjetas. Las tarjetas deben voltearse cuando se hace clic.",
             image: labReact,
             githubLink: "https://github.com/Sebas021210/Lab8-React",
-            link: "https://stw-23-9ad4e.web.app/"
+            link: "https://stw-23-9ad4e.web.app/",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3, mdiLanguageJavascript, mdiReact, mdiWebpack]
         },
         {
             title: "Disney+ Clone",
             text: "El proyecto consiste en escoger una página web que admiren y crear una copia idéntica del estilo del sitio.",
             image: DisneyClone,
             githubLink: "https://github.com/Sebas021210/Proyecto1-STW",
-            link: "https://proyecto1-stw-8c95b.web.app/"
+            link: "https://proyecto1-stw-8c95b.web.app/",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3, mdiLanguageJavascript, mdiReact, mdiSass, mdiWebpack]
         },
         {
             title: "Medicom",
             text: "El proyecto consiste en crear una página web que permita a los médicos y a los hospitales llevar un mejor control de los pacientes.",
             image: Medicom,
             githubLink: "https://github.com/Jskenpo/MediCom",
-            link: ""
+            link: "",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3, mdiLanguageJavascript]
         },
         {
             title: "TableTrek",
             text: "El proyecto consiste en crear una página web que permita a los usuarios buscar y reservar en los mejores restaurantes del país.",
             image: TableTrek,
             githubLink: "https://github.com/Teviets/TableTrek",
-            link: "http://18.220.61.52/"
+            link: "http://18.220.61.52/",
+            icons: [mdiLanguageHtml5, mdiLanguageCss3, mdiLanguageJavascript, mdiVuejs, mdiVuetify]
         },
     ];
 
@@ -132,7 +151,7 @@ function Cards() {
                             <Card
                                 style={{
                                     width: '20rem',
-                                    height: '17rem',
+                                    height: '19rem',
                                     background: '#2C2E3A',
                                     color: '#fff',
                                     marginBottom: '1rem'
@@ -149,6 +168,11 @@ function Cards() {
                                     }}
                                 >
                                     <Card.Text>{card.text}</Card.Text>
+                                    <div className="icon-container">
+                                        {card.icons.map((icon, iconIndex) => (
+                                            <Icon key={iconIndex} path={icon} size={1} color="#fff" />
+                                        ))}
+                                    </div>
                                 </Card.Body>
                                 <Card.Footer className="text-muted">
                                     <Button variant="primary" style={{ background: '#202435' }}>Más información</Button>
--- portafolio/src/views/home.js
+++ portafolio/src/views/home.js
@@ -1,24 +1,42 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import Header from "../components/header";
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import programming from "../images/Programming-amico.png";
+import Icon from '@mdi/react';
+import { mdiGithub } from '@mdi/js';
+import { mdiLinkedin } from '@mdi/js';
+import { mdiInstagram } from '@mdi/js';
+import { mdiTwitter } from '@mdi/js';
+import { mdiCodeTags } from '@mdi/js';
 import "./styles/home.css";
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleClickProyectos = () => {
+        navigate('/proyectos');
+    };
+
     return (
         <div id="Home">
             <div id="Header">
                 <Header />
             </div>
             <div className="bodyHome">
-                <Stack>
-                    <Avatar 
-                        alt="Remy Sharp" 
-                        src="https://img.freepik.com/vector-gratis/hombre-muestra-gesto-gran-idea_10045-637.jpg" 
-                        sx={{ width: 200, height: 200 }}
-                    />
-                </Stack>
-                <h1>Soy Sebastian Solorzano</h1>
+                <div className="textoPrincipal">
+                    <h1>Hola, soy Sebastián</h1>
+                    <h6>Bienvenidos a mi portafolio de la clase de Sistemas y Tecnologías Web, en el que podrán encontrar información sobre mí, mis proyectos más importantes y mis contactos.</h6>
+                    <div className="contactos">
+                        <a href="https://github.com/Sebas021210" target="_blank" rel="noopener noreferrer"><Icon path={mdiGithub} size={2} color="white"/></a>
+                        <a href="https://www.linkedin.com/in/sebasti%C3%A1n-jos%C3%A9-solorzano-p%C3%A9rez-7544b9205/" target="_blank" rel="noopener noreferrer"><Icon path={mdiLinkedin} size={2} color="white"/></a>
+                        <a href="https://www.instagram.com/sebass.sp/" target="_blank" rel="noopener noreferrer"><Icon path={mdiInstagram} size={2} color="white"/></a>
+                        <a href="https://twitter.com/sebas_tank10" target="_blank" rel="noopener noreferrer"><Icon path={mdiTwitter} size={2} color="white"/></a>
+                        <button onClick={handleClickProyectos} style={{ background: "none", border: "none", padding: 0, outline: "none" }}><Icon path={mdiCodeTags} size={2} color="white"/></button>
+                    </div>
+                </div>
+                <div className="imageProgramming">
+                    <img src={programming} alt="programming" width="650px" height="600px"></img>
+                </div>
             </div>
         </div>
     );
